016d52668fb1d9d870580bb4d509190e37709de3
Geoffroy Perriard <geoffroy.perriard@cpnv.ch>
2011-12-06 10:30:46 +0100

    SKI1 - Add the files from RoR with the webservice to provide the inscription

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkiSchool::Application.config.secret_token = '561a02a3559b55f4cd9a06b83e79bee8de181d8522031b0312f7829407a000e70e6afd00b72a5ead0ebe4f1a117575e5f6a7cb93e8e0c4a9a7763ccb33f0722c'
